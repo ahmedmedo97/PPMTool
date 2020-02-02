81f2f06016afd8589a792271f4c44359b6ff7894
Ahmed Almogtaba <mogtabaaa2013@gmail.com>
2020-02-02 22:51:08 +0200

    Refactor Project Controller

---
+++ PPMTOOL/src/main/java/io/mogtabaintellegence/services/MapValidationErrorService.java
@@ -0,0 +1,29 @@
+package io.mogtabaintellegence.services;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Interface 3 on 2/2/2020.
+ */
+@Service
+public class MapValidationErrorService {
+
+    public ResponseEntity<?> MapValidationService(BindingResult result) {
+        if (result.hasErrors()) {
+            Map<String,String> errorMap = new HashMap<>();
+            for (FieldError error:result.getFieldErrors())
+            {
+                errorMap.put(error.getField(),error.getDefaultMessage());
+            }
+            return new ResponseEntity<Map<String,String>>(errorMap, HttpStatus.BAD_REQUEST);
+        }
+        return null;
+    }
+}
