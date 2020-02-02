4ca8ed9e43b5e0d56bce9ecc4d64668832fff74e
Ahmed Almogtaba <mogtabaaa2013@gmail.com>
2020-02-02 18:39:01 +0200

    Project Object Validation Part one

--- PPMTOOL/src/main/java/io/mogtabaintellegence/web/ProjectController.java
+++ PPMTOOL/src/main/java/io/mogtabaintellegence/web/ProjectController.java
@@ -5,10 +5,13 @@ import io.mogtabaintellegence.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 /**
  * Created by Interface 3 on 2/2/2020.
@@ -21,7 +24,10 @@ public class ProjectController {
 
 
     @PostMapping("")
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project) {
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
+        if (result.hasErrors()) {
+            return new ResponseEntity<String>("Invalid Project Object" , HttpStatus.BAD_REQUEST);
+        }
         projectService.SaveOrUpdateProject(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
 
