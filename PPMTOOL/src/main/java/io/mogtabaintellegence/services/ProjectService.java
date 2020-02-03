985878d041628d2bbf9d3ce8705768269a6e4d1c
Ahmed Almogtaba <mogtabaaa2013@gmail.com>
2020-02-03 10:44:08 +0200

    Find Project By Identifier

--- PPMTOOL/src/main/java/io/mogtabaintellegence/services/ProjectService.java
+++ PPMTOOL/src/main/java/io/mogtabaintellegence/services/ProjectService.java
@@ -5,6 +5,7 @@ import io.mogtabaintellegence.exceptions.ProjectIdException;
 import io.mogtabaintellegence.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  * Created by Interface 3 on 2/2/2020.
@@ -23,7 +24,14 @@ public class ProjectService {
             throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase() + "' already exists");
 
         }
-
-       //return projectRepository.save(project);
     }
+
+    public Project findProjectByIdentifier(String projectId) {
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+        if (project == null) {
+            throw new ProjectIdException("Project ID '" + projectId + "' does not exists");
+        }
+        return project;
+    }
+
 }
--- PPMTOOL/src/main/java/io/mogtabaintellegence/web/ProjectController.java
+++ PPMTOOL/src/main/java/io/mogtabaintellegence/web/ProjectController.java
@@ -7,16 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+
 
 /**
  * Created by Interface 3 on 2/2/2020.
@@ -37,5 +31,11 @@ public class ProjectController {
         projectService.SaveOrUpdateProject(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
 
+    }
+
+    @GetMapping("/{projectId}")
+    public ResponseEntity<?> getProjectById(@PathVariable String projectId) {
+        Project project = projectService.findProjectByIdentifier(projectId);
+        return new ResponseEntity<Project>(project,HttpStatus.OK);
     }
 }
