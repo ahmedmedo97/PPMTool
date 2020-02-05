dd86ca511ce9564510045649066c81b315e38bee
Ahmed Almogtaba <mogtabaaa2013@gmail.com>
2020-02-05 08:23:55 +0200

    first functional component + react Router

---
+++ ppmtool-react-client/src/components/Project/AddProject.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class addProject extends Component {
+  render() {
+    return (
+      <div>
+        <h1>AddProject Form</h1>
+      </div>
+    );
+  }
+}
+export default addProject;
