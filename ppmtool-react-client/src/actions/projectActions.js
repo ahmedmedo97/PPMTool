61be14343295be5322829b54c1caa52d34d0e82a
Ahmed Almogtaba <mogtabaaa2013@gmail.com>
2020-02-16 21:31:23 +0200

    Routes to Project Board and add Project Task

--- ppmtool-react-client/src/actions/projectActions.js
+++ ppmtool-react-client/src/actions/projectActions.js
@@ -3,7 +3,7 @@ import { GET_ERRORS, GET_PROJECTS, GET_PROJECT, DELETE_PROJECT } from "./types";
 
 export const createProject = (project, history) => async dispatch => {
   try {
-    const res = await axios.post("/api/project", project);
+    await axios.post("/api/project", project);
     history.push("/dashboard");
     dispatch({
       type: GET_ERRORS,
@@ -43,7 +43,7 @@ export const deleteProject = id => async dispatch => {
       "Are you sure ? This will delete the project and all the data related to it"
     )
   ) {
-    const res = await axios.delete(`/api/project/${id}`);
+    await axios.delete(`/api/project/${id}`);
     dispatch({
       type: DELETE_PROJECT,
       payload: id
