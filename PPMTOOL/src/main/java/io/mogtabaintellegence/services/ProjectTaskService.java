e3dd006cd1d19b080c215dcfecd006975f9384d5
Ahmed Almogtaba <mogtabaaa2013@gmail.com>
2020-02-16 09:36:54 +0200

    Persist ProjectTask(Bug Fix Bending setPriority)

---
+++ PPMTOOL/src/main/java/io/mogtabaintellegence/services/ProjectTaskService.java
@@ -0,0 +1,43 @@
+package io.mogtabaintellegence.services;
+
+import io.mogtabaintellegence.domain.Backlog;
+import io.mogtabaintellegence.domain.ProjectTask;
+import io.mogtabaintellegence.repositories.BacklogRepository;
+import io.mogtabaintellegence.repositories.ProjectTaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Interface 3 on 2/15/2020.
+ */
+@Service
+public class ProjectTaskService {
+    @Autowired
+    private BacklogRepository backlogRepository;
+    @Autowired
+    private ProjectTaskRepository projectTaskRepository;
+
+    public ProjectTask addProjectTask(String projectIdentifier,ProjectTask projectTask){
+        //Exception : Project not found
+        //Pts to be added to a specific project , project ! = null,Bl exists
+        Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
+        //set bl to Pt
+        projectTask.setBacklog(backlog);
+        //we want our project sequence to be like this:  IDPRO-1 , IDPRO-2
+        Integer BacklogSequence = backlog.getPTSequence();
+        //update the BL sequence
+        BacklogSequence++;
+        //Add Sequence to project task
+        projectTask.setProjectSequence(projectIdentifier + "-" + BacklogSequence);
+        projectTask.setProjectIdentifier(projectIdentifier);
+        //Initial priority when priority is null
+//        if (projectTask.getPriority() == 0 || projectTask.getPriority()==null){
+//            projectTask.setPriority(3);
+//        }
+        //Initial status when status is null
+        if (projectTask.getStatus()=="" || projectTask.getStatus() == null) {
+            projectTask.setStatus("TO_DO");
+        }
+        return projectTaskRepository.save(projectTask);
+    }
+}
