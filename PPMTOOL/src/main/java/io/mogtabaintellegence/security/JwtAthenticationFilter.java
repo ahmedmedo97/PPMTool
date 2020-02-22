780e09bd43e00667e9a8cf5edb07ee1f988e8adb
Ahmed Almogtaba <mogtabaaa2013@gmail.com>
2020-02-22 17:45:20 +0200

    Token Generated + Custom JWT Filter to our token

---
+++ PPMTOOL/src/main/java/io/mogtabaintellegence/security/JwtAthenticationFilter.java
@@ -0,0 +1,65 @@
+package io.mogtabaintellegence.security;
+
+import io.mogtabaintellegence.domain.User;
+import io.mogtabaintellegence.services.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collections;
+
+import static io.mogtabaintellegence.security.SecurityConstants.HEADER_STRING;
+import static io.mogtabaintellegence.security.SecurityConstants.TOKEN_PREFIX;
+
+/**
+ * Created by Interface 3 on 2/22/2020.
+ */
+public class JwtAthenticationFilter extends OncePerRequestFilter {
+    @Autowired
+    private JwtTokenProvider tokenProvider;
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse
+            , FilterChain filterChain) throws ServletException, IOException {
+
+        try {
+            String jwt = getJWTFromRequest(httpServletRequest);
+            if (StringUtils.hasText(jwt)&&tokenProvider.validateToken(jwt)){
+                Long userId = tokenProvider.getUserIdFromJWT(jwt);
+                User userDetails = customUserDetailsService.loadUserById(userId);
+                UsernamePasswordAuthenticationToken authentication =
+                        new UsernamePasswordAuthenticationToken(
+                        userDetails,null, Collections.emptyList()
+                );
+
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(httpServletRequest));
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+
+        }catch (Exception ex){
+           logger.error("Could not set user authentication in security context",ex);
+        }
+
+        filterChain.doFilter(httpServletRequest,httpServletResponse);
+
+    }
+
+    private String getJWTFromRequest(HttpServletRequest request){
+      String bearerToken = request.getHeader(HEADER_STRING);
+
+      if (StringUtils.hasText(bearerToken)&&bearerToken.startsWith(TOKEN_PREFIX)){
+          return bearerToken.substring(7,bearerToken.length());
+      }
+      return null;
+    }
+}
--- PPMTOOL/src/main/java/io/mogtabaintellegence/security/SecurityConfig.java
+++ PPMTOOL/src/main/java/io/mogtabaintellegence/security/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static io.mogtabaintellegence.security.SecurityConstants.H2_URL;
 import static io.mogtabaintellegence.security.SecurityConstants.SIGN_UP_URLS;
@@ -35,6 +36,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private CustomUserDetailsService customUserDetailsService;
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Bean
+    public JwtAthenticationFilter jwtAthenticationFilter(){return new JwtAthenticationFilter();}
 
     @Override
     protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
@@ -71,5 +75,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(SIGN_UP_URLS).permitAll()
                 .antMatchers(H2_URL).permitAll()
                 .anyRequest().authenticated();
+        http.addFilterBefore(jwtAthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 }
