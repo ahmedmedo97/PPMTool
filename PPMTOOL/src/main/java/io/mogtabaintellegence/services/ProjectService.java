3e1d6210f3e7a3d4b20225a02954347f6e72a321
Ahmed Almogtaba <mogtabaaa2013@gmail.com>
2020-02-22 22:38:19 +0200

    Lock operations to specific user [read , update ,create,delete]

--- PPMTOOL/src/main/java/io/mogtabaintellegence/services/ProjectService.java
+++ PPMTOOL/src/main/java/io/mogtabaintellegence/services/ProjectService.java
@@ -2,9 +2,12 @@ package io.mogtabaintellegence.services;
 
 import io.mogtabaintellegence.domain.Backlog;
 import io.mogtabaintellegence.domain.Project;
+import io.mogtabaintellegence.domain.User;
 import io.mogtabaintellegence.exceptions.ProjectIdException;
+import io.mogtabaintellegence.exceptions.ProjectNotFoundException;
 import io.mogtabaintellegence.repositories.BacklogRepository;
 import io.mogtabaintellegence.repositories.ProjectRepository;
+import io.mogtabaintellegence.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,8 +22,25 @@ public class ProjectService {
     @Autowired
     private BacklogRepository backlogRepository;
 
-    public Project SaveOrUpdateProject(Project project) {
+    @Autowired
+    private UserRepository userRepository;
+
+    public Project SaveOrUpdateProject(Project project,String username) {
+
+        if (project.getId() !=null){
+            Project existingProject = projectRepository.findByProjectIdentifier(project.getProjectIdentifier());
+            if (existingProject !=null && (!existingProject.getProjectLeader().equals(username))){
+                throw new ProjectNotFoundException("Project not found in your account");
+            }else if (existingProject == null){
+                throw new ProjectNotFoundException("Project with ID: '"+ project.getProjectIdentifier()+"' cannot be updated because" +
+                        " it doesn't exist");
+            }
+        }
+
         try {
+            User user = userRepository.findByUsername(username);
+            project.setUser(user);
+            project.setProjectLeader(user.getUsername());
             project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
             if (project.getId() == null){
                 Backlog backlog = new Backlog();
@@ -39,25 +59,24 @@ public class ProjectService {
         }
     }
 
-    public Project findProjectByIdentifier(String projectId) {
+    public Project findProjectByIdentifier(String projectId,String username) {
         Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
         if (project == null) {
             throw new ProjectIdException("Project ID '" + projectId + "' does not exists");
         }
+        if (!project.getProjectLeader().equals(username)){
+            throw new ProjectNotFoundException("Project not found in your account");
+        }
         return project;
     }
 
-    public Iterable<Project> findAllProjects(){
-        return projectRepository.findAll();
+    public Iterable<Project> findAllProjects(String username){
+        return projectRepository.findAllByProjectLeader(username);
     }
 
-    public void deleteProjectByIdentifier(String projectId){
-        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+    public void deleteProjectByIdentifier(String projectId,String username){
 
-        if (project==null){
-            throw new ProjectIdException("Can not delete Project with ID '" + projectId + "'. This Project does not exist");
-        }
-        projectRepository.delete(project);
+        projectRepository.delete(findProjectByIdentifier(projectId,username));
 
     }
 
